2220c33ebd19211483192a757c5b01e0ef60015b
Amélie Garrido <liondore@yahoo.com>
2017-04-08 11:45:58 +0200

    My Django Girls app, first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+def hi(name):
+    print('Hi ' + name +'!')
+
+
+girls = ['You', 'Ola', 'Monica', 'Rachel', 'Phoebe']
+for name in girls:
+    hi(name)
+    print('Next girl')
+
+for i in range(1,6):
+    print(i)
